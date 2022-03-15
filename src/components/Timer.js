ab8c1dfaf7e85eed2680b9ac3d9ce67cbfd57f7b
Timothy Ayegbede <timtop99@gmail.com>
2022-03-15 17:11:53 +0100

    completed with fixes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,15 +5,27 @@ import Countdown from "react-countdown";
 const Timer = () => {
   const Completionist = () => <span>It's Mint Day!</span>;
 
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {days} days : {hours} hrs : {minutes} mins : {seconds} secs
+        </span>
+      );
+    }
+  };
+
   return (
     <>
       {/* <DateCountdown
         dateTo="March 21, 2022 00:00:00 EST"
         callback={() => alert("It's Mint time")}
       /> */}
-      <Countdown date={"2022-03-21T00:00:00"}>
-        <Completionist />
-      </Countdown>
+      <Countdown date={"2022-03-21T00:00:00"} renderer={renderer} />
     </>
   );
 };
